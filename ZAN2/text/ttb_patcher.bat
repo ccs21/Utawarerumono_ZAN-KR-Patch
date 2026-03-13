a230071d66908514df390610f7877ed3e47f2d21
ccs21 <ccs2121@naver.com>
2026-03-14 01:05:41 +0900

    -

---
+++ ZAN2/text/ttb_patcher.bat
@@ -0,0 +1,5 @@
+cd /d D:\git\Utawarerumono_ZAN-KR-Patch\ZAN2\text
+
+hangul2kanji_ttb_patcher.py --ttb text_ori.ttb --csv text.csv --mapping hangul_to_kanji_mapping_2350.csv --out-ttb "D:\PC Games\Utawarerumono ZAN 2\Text\text.ttb"
+
+pause
